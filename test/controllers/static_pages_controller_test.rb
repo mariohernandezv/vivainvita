9e8fbf82cdbb058f8cb41fa5b479677a1bdf5006
Mario Hernández <mariohernandezv@yahoo.com.ar>
2017-04-17 19:56:13 -0500

    Definiendo paginas estaticas

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,8 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get contact" do
+    get static_pages_contact_url
     assert_response :success
   end
 

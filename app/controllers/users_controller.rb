10f227ebcbf5258518994488bae9b9f31cd7a7a2
Mario Hernández <mariohernandezv@yahoo.com.ar>
2017-04-20 09:42:35 -0500

    Implementando login basico

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Bienvenido a Viva Invitaciones!"
       redirect_to @user
     else

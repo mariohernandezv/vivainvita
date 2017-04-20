7467adcae94d3ae9938dc2e1e8a70c969326e51c
Mario Hernández <mariohernandezv@yahoo.com.ar>
2017-04-20 09:02:31 -0500

    Terminando el registro de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170420131934) do
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "price"
-    t.string   "color"
-    t.string   "size"
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_products_on_name", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"

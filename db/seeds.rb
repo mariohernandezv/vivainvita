d9b953143f9978e4855a226f21aa5b3fcf6b5334
Mario Hernández <mariohernandezv@yahoo.com.ar>
2017-05-03 20:25:05 -0500

    Terminando edicion, index, y eiminar usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "Mario Hernandez",
+             email: "mariohernandezv@yahoo.com.ar",
+             password:              "11235813",
+             password_confirmation: "11235813", 
+             admin: true)
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+

037d1b81115c69fa684bae47ba3215c092427770
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2017-08-23 15:36:46 -0500

    Agregando form de contacto

--- Gemfile
+++ Gemfile
@@ -20,6 +20,10 @@ gem 'jquery-rails', '4.1.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.4.1'
 
+#Para el ContactForm
+gem 'mail_form'
+gem 'simple_form'
+
 group :development, :test do
   gem 'sqlite3', '1.3.12'
   gem 'byebug',  '9.0.0', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
+
+  #Para contactos
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
+
+  
 #  get    '/signup',  to: 'users#new'
 #  get    '/login',   to: 'sessions#new'
 #  post   '/login',   to: 'sessions#create'

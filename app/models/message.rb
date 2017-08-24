059a59512948cc435946e6eb1b2816b0739b3dbe
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2017-08-24 16:16:50 -0500

    Arreglando el estilo

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,6 +12,6 @@ class Message
 
   validates :email, presence: { message: "no puede estar en blanco" }, format: { with: VALID_EMAIL_REGEX, message: "no parece ser una dirección válida" }
 
-  validates :mensaje, presence: { message: "no puede estar en blanco" }, length: { maximum: 5000, message: "no debe exceder los 5000 caracteres" }
+  validates :mensaje, presence: { message: "no puede estar en blanco" }, length: { maximum: 10000, message: "no debe exceder los 10000 caracteres" }
 
 end

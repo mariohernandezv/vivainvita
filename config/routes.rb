cd4b8ad0f69205a9096ee9def435fece07bb9871
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2017-08-23 20:00:36 -0500

    Contacto

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
+#  get    '/contact', to: 'static_pages#contact'
+
+  get 'contact', to: 'messages#new', as: 'contact'
+  post 'contact', to: 'messages#create'
 #  get    '/signup',  to: 'users#new'
 #  get    '/login',   to: 'sessions#new'
 #  post   '/login',   to: 'sessions#create'

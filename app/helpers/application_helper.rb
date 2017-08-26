460f98614e3c5ca271454f9c5005a7c9becde244
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2017-08-25 19:54:12 -0500

    Primeros bocetos de fotografia

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 	  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Viva Invitaciones"
+    base_title = "Viva"
     if page_title.empty?
-      base_title
+      base_title + ", invitaciones y fotografía"
     else
       page_title + " | " + base_title
     end

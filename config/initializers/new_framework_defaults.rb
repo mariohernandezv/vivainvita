78b92297aa88a715a4cff915204f2be9d3550266
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2017-08-30 11:13:07 -0500

    quitando cookies

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -5,7 +5,7 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = true
+Rails.application.config.action_controller.per_form_csrf_tokens = false
 
 # Enable origin-checking CSRF mitigation. Previous versions had false.
 Rails.application.config.action_controller.forgery_protection_origin_check = true
@@ -21,4 +21,4 @@ Rails.application.config.active_record.belongs_to_required_by_default = true
 ActiveSupport.halt_callback_chains_on_return_false = false
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
+Rails.application.config.ssl_options = { hsts: { subdomains: false } }

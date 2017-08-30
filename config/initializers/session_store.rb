4a0e014a0795ef6bef208031400e989f9cb1fdeb
Mario Hernandez <mariohernandezv@yahoo.com.ar>
2017-08-30 10:16:40 -0500

    Quitando cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_vivainvita_session'
+#Para generar el cookie
+#Rails.application.config.session_store :cookie_store, key: '_vivainvita_session'
